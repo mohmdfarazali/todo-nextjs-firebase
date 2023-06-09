86a47c992aa1382b5259d1b18a87d3087409522f
Faraz <mohmdfarazali@gmail.com>
2023-06-09 19:28:35 +0530

    adding favicon

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Task Savvy</title>
+        <link rel="icon" href="/logo.png" />
       </Head>
       <AuthUserProvider>
         <Component {...pageProps} />
